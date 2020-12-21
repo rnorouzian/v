fbfa7877800207fdf2f6f2d067874c3034027545
Reza Norouzian <rnorouzian@gmail.com>
2020-12-21 01:34:48 -0600

    Add files via upload

---
+++ v.r
@@ -0,0 +1,28 @@
+
+Break = "\n<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>\n"
+
+notice = "   Welcome to 'EDP 380C.12 Survey of Multivariate Methods'.
+   Programs developed by Reza Norouzian, Copyright (C) 2019-present"
+
+message(Break, notice, Break)
+
+
+
+
+
+
+
+need <- c('car','psych','reshape','tidyverse','lme4','nlme','MASS','CCA',
+          'parallel','rela','gplots','ICSNP','mvtnorm','mvnormtest','normtest',
+          'normwhn.test','nortest','biotools','effects','ez','yacca')
+
+not.have <- need[!(need %in% installed.packages()[,"Package"])]
+if(length(not.have)) install.packages(not.have)
+
+
+suppressMessages({ 
+  
+  for(i in need){
+    library(i, character.only = TRUE)
+  }
+})  
