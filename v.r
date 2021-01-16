9f0995b2710ba7bde66ded7d7c1fd15c6454d2dd
Reza Norouzian <rnorouzian@gmail.com>
2021-01-15 20:25:21 -0600

    Update v.r

--- v.r
+++ v.r
@@ -71,10 +71,141 @@ if(inherits(fit, c("lmerMod", "lmerModLmerTest", "lme4"))){
 
 }
                         
-#=======================================================================                        
+#=======================================================================
+                        
+do_context <- function(data, context_vars, group_id){
+  
+  all_names <- names(data)
+  
+  id <- grep("id|group|grp", all_names, value = TRUE, ignore.case = TRUE)
+  
+  if(!all(group_id %in% all_names)) { 
+    
+    stop(paste(toString(dQuote(group_id)), "not found for 'group_id' in the 'data'.", if(length(id)>0) 
+      paste("\nPossibly you meant to use", toString(dQuote(id)), "as 'group_id', no?")), call. = FALSE) 
+    
+    }
+  
+  ok <- context_vars %in% all_names
+  
+  if(!all(ok)) message(paste("\n", toString(dQuote(context_vars[!ok])), "not found in the 'data' thus ignored."))
+  
+  context_vars <- context_vars[ok] 
+  
+  dum_vars <- all_names[sapply(data, function(i)is.character(i)|is.factor(i))]
+  
+  dum_names <- context_vars[context_vars %in% dum_vars]
+  
+  is_dum <- length(dum_names) > 0
+  
+  num_names <- context_vars[!(context_vars %in% dum_vars)]
+  
+  is_num <- length(num_names) > 0
+  
+  
+  if(is_num){
+    data <- data %>%
+      group_by(across(all_of(group_id))) %>%                          
+      mutate(across(all_of(num_names), list(wthn = ~ . - mean(.), btw = ~ mean(.)))) %>% 
+      as.data.frame()
+  }
+  
+  
+  if(is_dum){
+    data <- data %>%
+      dummy_cols(select_columns = dum_names) %>% 
+      group_by(across(all_of(group_id))) %>%                          
+      mutate(across(starts_with(paste0(dum_names, "_")), list(wthn = ~ . - mean(.), btw = ~ mean(.)))) %>% 
+      as.data.frame()
+  }
+  
+  return(data)
+}
+
+#=================================================================================================================================  
+                               
+                               
+hetro_var <- function(fit){
+  
+    if(!inherits(fit, c("lme", "gls"))) stop("Only 'lme()' & 'gls()' models are accepted.", call. = FALSE)
+  
+  coef(fit$modelStruct$varStruct, uncons = FALSE, allCoef = TRUE)
+  
+}
+     
+#=================================================================================================================================
+
+                               
+rho_lme <- function(fit) {
+  
+  if(!inherits(fit, c("lme", "gls"))) stop("Only 'lme()' & 'gls()' models are accepted.", call. = FALSE)
+  
+  coef(fit$modelStruct$corStruct, uncons = FALSE, allCoef = TRUE)
+
+}                               
+ 
+                               
+#=================================================================================================================================
+                               
+cov_str <- function(fit, cov = TRUE, time_var = "time", hlm = TRUE){
+  
+  rho <- rho_lme(fit)
+  hetro <- hetro_var(fit)
+  sig <- sigma(fit)
+  dat <- getData(fit)
+  if(!(time_var %in% names(dat))) stop("Your 'time_var' doesn't exist in your data.", call. = FALSE)
+  time_vals <- unique(dat[[time_var]])
+  
+  if(is.null(rho) & is.null(hetro)) return(id_cor(fit, cov = cov, time_var = time_var, hlm = hlm))
+  if(is.null(rho) & !is.null(hetro)) {
+    
+    res <- id_cor(fit, cov = cov, time_var = time_var, hlm = hlm)
+    diag(res) <- sum_ranef_var(fit)+(sig*hetro)^2
+    return(res)  
+    
+  }
+  corm <- corMatrix(fit$modelStruct$corStruct)[[1]]
+  
+  res <- corm*sig^2+if(hlm)sum_ranef_var(fit) else 0 *if(is.null(hetro)) 1 else t(t(hetro))%*%t(hetro)
+  if(!is.null(hetro)) diag(res) <- sum_ranef_var(fit)+(sigma(fit)*hetro_var(fit))^2
+    
+  if(!cov) res <- cov2cor(res)
+  rownames(res) <- colnames(res) <- paste0(time_var,time_vals)
+  return(res)
+  
+  }                               
+                               
+#=================================================================================================================================  
+                               
+id_cor <- function(fit, cov = TRUE, time_var = "time", hlm = TRUE){
+  
+  sig <- sigma(fit)^2
+  vrs <- as.numeric(VarCorr(fit)[,"Variance"])
+  time_vals <- unique(getData(fit)[[time_var]])
+  steps <- length(time_vals)
+  x <- diag(steps)
+  res <- sig*x
+  
+  diag(res) <- sum_ranef_var(fit, resid = TRUE) 
+  res[col(res)!=row(res)] <- if(hlm) sum_ranef_var(fit) else 0
+  
+  rownames(res) <- colnames(res) <- paste0(time_var,time_vals)
+  if(!cov) res <- cov2cor(res)
+  return(res)
+} 
+                               
+#=================================================================================================================================
+           
+sum_ranef_var <- function(fit, resid = FALSE){
+vrs <- as.numeric(VarCorr(fit)[,"Variance"])
+sum(if(resid) vrs else rev(vrs)[-1], na.rm = TRUE)
+}                               
+                                                       
+                        
+#========================================================================                        
                                              
-needzzsf <- c('car','psych','reshape','tidyverse','lme4','nlme','MASS','CCA','matrixcalc', 'mvoutlier', 'vegan', 'haven',
-          'parallel','rela','gplots','ICSNP','mvtnorm','mvnormtest','normtest', 'micompr', 'heplots', 'HSAUR', 'bbmle',
+needzzsf <- c('car','psych','reshape','tidyverse','lme4','nlme','MASS','CCA','matrixcalc', 'mvoutlier', 'vegan', 'haven', 'fastDummies', "emmeans",
+          'parallel','rela','gplots','ICSNP','mvtnorm','mvnormtest','normtest', 'micompr', 'heplots', 'HSAUR', 'bbmle', 'jtools', 'stargazer', 'interactions',
           'normwhn.test','nortest','biotools','effects','ez','yacca')
 
                         
