c84f8611793eb6803898ad93b0e28a025bffbd34
Reza Norouzian <rnorouzian@gmail.com>
2021-01-15 18:23:40 -0600

    Update v.r

--- v.r
+++ v.r
@@ -56,7 +56,7 @@ gen_mv_data <- function(n, mu1, mu2, mu3 = NULL, mu4 = NULL, cov1, cov2 = cov1, 
 
 
 
-need <- c('car','psych','reshape','tidyverse','lme4','nlme','MASS','CCA','matrixcalc', 'mvoutlier', 'vegan',
+need <- c('car','psych','reshape','tidyverse','lme4','nlme','MASS','CCA','matrixcalc', 'mvoutlier', 'vegan', 'haven',
           'parallel','rela','gplots','ICSNP','mvtnorm','mvnormtest','normtest', 'micompr', 'heplots', 'HSAUR',
           'normwhn.test','nortest','biotools','effects','ez','yacca')
 
