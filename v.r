c7eaf869fccc0783b8f8b2994dda6eed08972cbe
Reza Norouzian <rnorouzian@gmail.com>
2021-01-23 11:01:44 -0600

    Update v.r

--- v.r
+++ v.r
@@ -247,8 +247,10 @@ environment(plot.efflist) <- asNamespace("effects")
     
     
 #========================================================================                        
-                                             
-needzzsf <- c('car','psych','reshape','tidyverse','lme4','nlme','MASS','CCA','matrixcalc', 'mvoutlier', 'vegan', 'haven', 'fastDummies', "emmeans", 'sjPlot', 'lmerTest', 'reghelper',
+
+# 'sjPlot', 'sjstats'    
+    
+needzzsf <- c('car','psych','reshape','tidyverse','lme4','nlme','MASS','CCA','matrixcalc', 'mvoutlier', 'vegan', 'haven', 'fastDummies', "emmeans", 'lmerTest', 'reghelper',
           'parallel','rela','gplots','ICSNP','mvtnorm','mvnormtest','normtest', 'micompr', 'heplots', 'HSAUR', 'bbmle', 'jtools', 'stargazer', 'interactions',
           'normwhn.test','nortest','biotools','effects','ez','yacca')
 
