1ff2a651948d35bb452c3757104c046f0258b68b
Reza Norouzian <rnorouzian@gmail.com>
2021-04-28 21:42:11 -0500

    Update v.r

--- v.r
+++ v.r
@@ -691,7 +691,7 @@ exam.efa <- function(x, factors, data = NULL, covmat = NULL, n.obs = NA,
 # 'sjPlot', 'sjstats'    
     
 needzzsf <- c('car','psych','tidyverse','lme4','nlme','MASS','matrixcalc', 'haven', 'lmerTest', 'reghelper',
-          'parallel','rela','bbmle', 'jtools','interactions','GPArotation',
+          'parallel','rela','bbmle', 'jtools','interactions','GPArotation', 'rstatix',
           'effects', 'effectsize', "paran", 'Hotelling','ICC')
 
                         
